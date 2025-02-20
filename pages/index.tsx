fe8f987416086c7edd08702b418bb7596e02d338
Benjampo <benjaminporchet.pro@gmail.com>
2025-02-20 09:26:58 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="px-0">
+            className="px-0 flex flex-col gap-16">
             <Head>
                 <title>Benjamin Porchet | Full-stack Developper</title>
             </Head>
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
                     transition={{ delay: 0.1 }}
                     animate={{ opacity: 1, y: 0 }}
                     className=" mt-2 font-medium text-2xl text-center text-gray-400">
-                    I’m Benjamin, Front-End developper
+                    I’m Benjamin, Full-Stack developper
                 </motion.h1>
             </section>
             <section>
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
                                         initial={{ opacity: 0, y: 50 }}
                                         whileInView={{ opacity: 1, y: 0 }}
                                         whileHover={{ scale: 1.02 }}
-                                        className=" border border-2 border-blue-200 relative w-full md:w-5/6  h-[15rem] md:h-[40rem] rounded-xl my-shadow overflow-hidden">
+                                        className="border-2 border-blue-200 relative w-full md:w-5/6  h-[15rem] md:h-[40rem] rounded-xl my-shadow overflow-hidden">
                                         <figure className="absolute top-0 left-0 right-0 bottom-0 cursor-pointer w-full h-full">
                                             <Image
                                                 layout={'fill'}
