63de6bfe54b47524d4a1643805273a1a2a621c21
Benjampo <benjaminporchet.pro@gmail.com>
2025-09-09 11:30:59 +0200

    Update age in About section from 23 to 26 years old

--- pages/about.tsx
+++ pages/about.tsx
@@ -67,7 +67,7 @@ function About() {
                     </div>
 
                     <p className="font-normal  ">
-                        Full-stack nodeJs Developer specialized in front-end . 23 years old, I am
+                        Full-stack nodeJs Developer specialized in front-end . 26 years old, I am
                         passionate about creating and implementing solutions that make users lives
                         easier. <span className="block mb-2" />I started my journey as a Front-end
                         Developer in 2017, while studying at CPNV in Switzerland. In 2020 I did a
